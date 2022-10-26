a7fec4f4d4e64dedd686d3f9ce39ff06366bf5dc
jcacc <joe@accardi.xyz>
2022-10-25 20:56:35 -0400

    first commit

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 3. Create a new object and then create another object that inherits from it.  Each object should have at least 1 property.  Example from the pet store: first class is Animal, second class is Cat and it inherits from Animal.
 4. Paste the following template in the Program file:
 
-```
+
 Console.WriteLine("How many records do you want to add? ");
 var numberOfRecords = int.Parse(Console.ReadLine());
 
@@ -25,7 +25,7 @@ for (int i = 0; i < numberOfRecords; i++)
 // Print out the list of records using Console.WriteLine()
 
 
-```
+
  
 5. Replace “MyClass” with the child class that you made in step 2.  
 6. You should allow the user to add data for every property in your class.  So with the cat example, the user should be able to add values for every property of Cat which includes the properties from Animal.
---
+++ SDC1-KC2/SDC1-KC2/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+
+using SDC1_KC2;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+
+internal class Program
+{ 
+   private Animals 
+
+    private static void Main(string[] args)
+    {
+       
+
+
+        Console.WriteLine("How many records do you want to add? ");
+        var numberOfRecords = int.Parse(Console.ReadLine());
+       
+
+        var recordList = new List<Animals>();
+        for (int i = 0; i < numberOfRecords; i++)
+        {
+            // In this loop, populate the object's properties using Console.ReadLine()
+            var Animal = new Animals();
+
+            WriteLine("Enter the animal's name");
+            string animalName = ReadLine();
+
+            WriteLine("")
+
+
+
+
+            recordList.Add(Animal);
+        }
+
+        // Print out the list of records using Console.WriteLine()
+
+    }
+}
