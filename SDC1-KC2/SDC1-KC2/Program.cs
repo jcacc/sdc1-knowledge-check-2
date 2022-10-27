1dccb7e722385824ae93c9ff740e09afa39b96f2
jcacc <joe@accardi.xyz>
2022-10-26 20:27:44 -0400

    figured it out, will add more properties later

--- SDC1-KC2/SDC1-KC2/Program.cs
+++ SDC1-KC2/SDC1-KC2/Program.cs
@@ -6,37 +6,47 @@ using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using static System.Console;
 
-internal class Program
-{ 
-   private Animals 
 
-    private static void Main(string[] args)
+namespace SDC1_KC2
+{
+
+
+    internal class Program
     {
-       
 
 
-        Console.WriteLine("How many records do you want to add? ");
-        var numberOfRecords = int.Parse(Console.ReadLine());
-       
-
-        var recordList = new List<Animals>();
-        for (int i = 0; i < numberOfRecords; i++)
+        private static void Main(string[] args)
         {
-            // In this loop, populate the object's properties using Console.ReadLine()
-            var Animal = new Animals();
-
-            WriteLine("Enter the animal's name");
-            string animalName = ReadLine();
-
-            WriteLine("")
 
 
 
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
 
-            recordList.Add(Animal);
+
+            var animalList = new List<Animal>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var Animal = new Animal();
+
+                WriteLine("Enter the animal's name");
+                Animal.Name = ReadLine();
+
+                
+
+
+
+
+
+                animalList.Add(new Animal() { Name = Animal.Name });
+            }
+
+            // Print out the list of records using Console.WriteLine()
+            foreach (var animal in animalList)
+            {
+                WriteLine("Animal Name: "+ animal.Name);
+            }
         }
-
-        // Print out the list of records using Console.WriteLine()
-
     }
 }
