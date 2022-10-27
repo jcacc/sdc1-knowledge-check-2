1bd88d095d4f57774a18de31d326fed808a456d9
jcacc <joe@accardi.xyz>
2022-10-26 20:55:01 -0400

    changed to shirts

--- SDC1-KC2/SDC1-KC2/Program.cs
+++ SDC1-KC2/SDC1-KC2/Program.cs
@@ -20,32 +20,53 @@ namespace SDC1_KC2
 
 
 
-            Console.WriteLine("How many records do you want to add? ");
+            Console.WriteLine("How many shirts do you want to add? ");
             var numberOfRecords = int.Parse(Console.ReadLine());
 
 
-            var animalList = new List<Animal>();
+            var shirtList = new List<Shirt>();
             for (int i = 0; i < numberOfRecords; i++)
             {
                 // In this loop, populate the object's properties using Console.ReadLine()
-                var Animal = new Animal();
+                var entry = new Shirt();
 
-                WriteLine("Enter the animal's name");
-                Animal.Name = ReadLine();
+                WriteLine("Enter the shirt brand: ");
+                entry.Brand = ReadLine();
 
-                
+                WriteLine("Enter a short description of the design: ");
+                entry.DesignDescription = ReadLine();
+
+                WriteLine("Enter the color of shirt: ");
+                entry.Color = ReadLine();
+
+                WriteLine("What size is the shirt?: (S, M, L, XL)");
+                entry.Size = ReadLine().ToUpper();
+
+                            
 
 
 
 
 
-                animalList.Add(new Animal() { Name = Animal.Name });
+               shirtList.Add(new Shirt() { Brand = entry.Brand, DesignDescription = entry.DesignDescription, Color = entry.Color, Size = entry.Size });
+
+
             }
-
+            Clear();
+            WriteLine("These are the shirts you entered...");
             // Print out the list of records using Console.WriteLine()
-            foreach (var animal in animalList)
+            foreach (var entry in shirtList)
             {
-                WriteLine("Animal Name: "+ animal.Name);
+                
+                
+                WriteLine("Shirt Brand: " + entry.Brand);
+                WriteLine("Description: " + entry.DesignDescription);
+                WriteLine("Color: " + entry.Color);
+                WriteLine("Size: " + entry.Size);
+                WriteLine("");
+
+
+
             }
         }
     }
---
+++ SDC1-KC2/SDC1-KC2/Shirt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDC1_KC2
+{
+    public class Shirt
+    {
+        public string Brand { get; set; }
+        public string DesignDescription { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+
+
+
+
+
+
+    }
+}
